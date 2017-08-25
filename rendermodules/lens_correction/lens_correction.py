3e70cfc0cf601dcdd8687165f582fc448aeb00ba
RussTorres <russtorres@gmail.com>
2017-08-25 12:45:45 -0700

    lenscorrection: document parameters, PEP8

--- rendermodules/lens_correction/lens_correction.py
+++ rendermodules/lens_correction/lens_correction.py
@@ -1,81 +1,125 @@
-import os
-import json
 import subprocess
 from argschema import ArgSchema, ArgSchemaParser
 from argschema.fields import Bool, Float, Int, Nested, Str
 from argschema.schemas import DefaultSchema
-import renderapi
+
 
 class SIFTParameters(DefaultSchema):
-    initialSigma = Float(required=True,
-        description='')
-    steps = Int(required=True,
-        description='')
-    minOctaveSize = Int(required=True,
-        description='')
-    maxOctaveSize = Int(required=True,
-        description='')
-    fdSize = Int(required=True,
-        description='')
-    fdBins = Int(required=True,
-        description='')
+    initialSigma = Float(
+        required=True,
+        description="initial sigma value for SIFT's gaussian blur, in pixels")
+    steps = Int(
+        required=True,
+        description='steps per SIFT scale octave')
+    minOctaveSize = Int(
+        required=True,
+        description='minimum image size used for SIFT octave, in pixels')
+    maxOctaveSize = Int(
+        required=True,
+        description='maximum image size used for SIFT octave, in pixels')
+    fdSize = Int(
+        required=True,
+        description='SIFT feature descriptor size')
+    fdBins = Int(
+        required=True,
+        description='SIFT feature descriptor orientation bins')
+
 
 class AlignmentParameters(DefaultSchema):
-    rod = Float(required=True,
-        description='')
-    maxEpsilon = Float(required=True,
-        description='')
-    minInlierRatio = Float(required=True,
-        description='')
-    minNumInliers = Int(required=True,
-        description='')
-    expectedModelIndex = Int(required=True,
-        description='')
-    multipleHypotheses = Bool(required=True,
-        description='')
-    rejectIdentity = Bool(required=True,
-        description='')
-    identityTolerance = Float(required=True,
-        description='')
-    tilesAreInPlace = Bool(required=True,
-        description='')
-    desiredModelIndex = Int(required=True,
-        description='')
-    regularize = Bool(required=True,
-        description='')
-    maxIterationsOptimize = Int(required=True,
-        description='')
-    maxPlateauWidthOptimize = Int(required=True,
-        description='')
-    dimension = Int(required=True,
-        description='')
-    lambdaVal = Float(required=True,
-        description='')
-    clearTransform = Bool(required=True,
-        description='')
-    visualize = Bool(required=True,
-        description='')
+    rod = Float(
+        required=True,
+        description='ratio of distances for matching SIFT features')
+    maxEpsilon = Float(
+        required=True,
+        description='maximum acceptable epsilon for RANSAC filtering')
+    minInlierRatio = Float(
+        required=True,
+        description='minimum inlier ratio for RANSAC filtering')
+    minNumInliers = Int(
+        required=True,
+        description='minimum number of inliers for RANSAC filtering')
+    expectedModelIndex = Int(
+        required=True,
+        description=('expected model for RANSAC filtering, 0=Translation, '
+                     '1="Rigid", 2="Similarity", 3="Affine"'))
+    multipleHypotheses = Bool(
+        required=True,
+        description=('Utilize RANSAC filtering which allows '
+                     'fitting multiple hypothesis models'))
+    rejectIdentity = Bool(
+        required=True,
+        description=('Reject Identity model (constant background) '
+                     'up to a tolerance defined by identityTolerance'))
+    identityTolerance = Float(
+        required=True,
+        description='Tolerance to which Identity should rejected, in pixels')
+    tilesAreInPlace = Bool(
+        required=True,
+        description=('Whether tile inputs to TrakEM2 are in place to be '
+                     'compared only to neighboring tiles.'))
+    desiredModelIndex = Int(
+        required=True,
+        description=('Affine homography model which optimization will '
+                     'try to fit, 0="Translation", 1="Rigid", '
+                     '2="Similarity", 3="Affine"'))
+    # TODO no input for regularizer model, defaults to Rigid regularization
+    regularize = Bool(
+        required=True,
+        description=('Whether to regularize the desired model with '
+                     'the regularizer model by lambda'))
+    maxIterationsOptimize = Int(
+        required=True,
+        description='Max number of iterations for optimizer')
+    maxPlateauWidthOptimize = Int(
+        required=True,
+        description='Maximum plateau width for optimizer')
+    dimension = Int(
+        required=True,
+        description=('Dimension of polynomial kernel to fit '
+                     'for distortion model'))
+    lambdaVal = Float(
+        required=True,
+        description=('Lambda parameter by which the regularizer model '
+                     'affects fitting of the desired model'))
+    clearTransform = Bool(
+        required=True,
+        description=('Whether to remove transforms from tiles '
+                     'before SIFT matching.  Not recommended'))
+    visualize = Bool(
+        required=True,
+        description=('Whether to have TrakEM2 visualize the lens '
+                     'correction transform.  Not recommended when '
+                     'running through xvfb'))
+
 
 class LensCorrectionParameters(ArgSchema):
-    manifest_path = Str(required=True,
+    manifest_path = Str(
+        required=True,
         description='path to manifest file')
-    project_path = Str(required=True,
+    project_path = Str(
+        required=True,
         description='path to project directory')
-    fiji_path = Str(required=True,
+    fiji_path = Str(
+        required=True,
         description='path to FIJI')
-    grid_size = Int(required=True,
-        description='')
+    grid_size = Int(
+        required=True,
+        description=('maximum row and column to form square '
+                     'subset of tiles starting from zero (based on filenames '
+                     'which end in "\{row\}_\{column\}.tif")'))
     SIFT_params = Nested(SIFTParameters)
     align_params = Nested(AlignmentParameters)
 
+
 class LensCorrectionModule(ArgSchemaParser):
     def __init__(self, *args, **kwargs):
-        super(LensCorrectionModule, self).__init__(schema_type = 
-            LensCorrectionParameters, *args, **kwargs)
+        super(LensCorrectionModule, self).__init__(
+            schema_type=LensCorrectionParameters, *args, **kwargs)
 
     def run(self):
         # command line argument for beanshell script
-        bsh_call = ["xvfb-run",
+        bsh_call = [
+            "xvfb-run",
             "-a",
             self.args['fiji_path'],
             "-Xms20g", "-Xmx20g", "-Xincgc",
@@ -99,10 +143,10 @@ class LensCorrectionModule(ArgSchemaParser):
             "-Dtaip=" + str(self.args['align_params']['tilesAreInPlace']),
             "-Ddmi=" + str(self.args['align_params']['desiredModelIndex']),
             "-Dreg=" + str(self.args['align_params']['regularize']),
-            "-Dmio=" + 
-                str(self.args['align_params']['maxIterationsOptimize']),
-            "-Dmpwo=" + 
-                str(self.args['align_params']['maxPlateauWidthOptimize']),
+            "-Dmio={}".format(
+                self.args['align_params']['maxIterationsOptimize']),
+            "-Dmpwo={}".format(
+                self.args['align_params']['maxPlateauWidthOptimize']),
             "-Ddim=" + str(self.args['align_params']['dimension']),
             "-Dlam=" + str(self.args['align_params']['lambdaVal']),
             "-Dctrans=" + str(self.args['align_params']['clearTransform']),
@@ -111,7 +155,8 @@ class LensCorrectionModule(ArgSchemaParser):
             "run_lens_correction.bsh"]
 
         subprocess.call(bsh_call)
-        
+
+
 if __name__ == '__main__':
     example_input = {
         "manifest_path": "/home/samk/projects/lens_correction/Wij_Set_594451332/594089217_594451332/_trackem_20170502174048_295434_5LC_0064_01_20170502174047_reference_0_.txt",
@@ -147,5 +192,5 @@ if __name__ == '__main__':
         }
     }
 
-    module = LensCorrectionModule(input_data = example_input)
+    module = LensCorrectionModule(input_data=example_input)
     module.run()
