af5a715bff5f0663057b3289c65ac908cb8fdbe4
forrest collman <forrest.collman@gmail.com>
2017-08-31 18:48:25 -0700

    pointed to new test data location

--- tests/test_calc_lens_correction.py
+++ tests/test_calc_lens_correction.py
@@ -4,9 +4,9 @@ from rendermodules.lens_correction.lens_correction import LensCorrectionModule
 @pytest.fixture(scope='session',autouse=True)
 def test_lens_correction():
     example_input = {
-        "manifest_path": "/allen/programs/celltypes/workgroups/em-connectomics/samk/lc_test_data/Wij_Set_594451332/594089217_594451332/_trackem_20170502174048_295434_5LC_0064_01_20170502174047_reference_0_.txt",
-        "project_path": "/allen/programs/celltypes/workgroups/em-connectomics/samk/lc_test_data/Wij_Set_594451332/594089217_594451332/",
-        "fiji_path": "/allen/programs/celltypes/workgroups/em-connectomics/samk/Fiji.app/ImageJ-linux64",
+        "manifest_path": "/allen/aibs/shared/image_processing/volume_assembly/lc_test_data/Wij_Set_594451332/594089217_594451332/_trackem_20170502174048_295434_5LC_0064_01_20170502174047_reference_0_.txt",
+        "project_path": "/allen/aibs/shared/image_processing/volume_assembly/lc_test_data/Wij_Set_594451332/594089217_594451332/",
+        "fiji_path": "/allen/aibs/shared/image_processing/volume_assembly/Fiji.app",
         "grid_size": 3,
         "heap_size": 20,
         "outfile": "test_LC.json",
