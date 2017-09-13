b852ec1788d0c230dcc141a946a8b5c091e7207a
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-09-13 11:39:11 -0700

    debugging rough alignment code

--- rendermodules/rough_align/apply_rough_alignment_transform_to_montage_stack.py
+++ rendermodules/rough_align/apply_rough_alignment_transform_to_montage_stack.py
@@ -17,6 +17,7 @@ example = {
         "host": "http://em-131fs",
         "port": 8998,
         "owner": "gayathri",
+        "project": "MM2",
         "client_scripts": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/render_20170613/render-ws-java-client/src/main/scripts"
     },
     "montage_stack": "mm2_acquire_8bit_reimage_clone",
@@ -26,7 +27,7 @@ example = {
     "tilespec_directory": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/scratch/rough/jsonFiles",
     "set_new_z": "False",
     "minZ": 1015,
-    "maxZ": 1118,
+    "maxZ": 1015,
     "scale": 0.025,
     "pool_size": 10
 }
@@ -37,6 +38,8 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
     newz = Z[1]
 
     try:
+        print z
+        print "A"
         # get lowres stack tile specs
         lowres_ts = render.run(
                             renderapi.tilespec.get_tile_specs_from_z,
@@ -57,7 +60,7 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
 
         # get the lowres stack rough alignment transformation
         tforms = lowres_ts[0].tforms
-
+        print "B"
         d = tforms[0].to_dict()
         dsList = d['dataString'].split()
         v0 = float(dsList[0])*scale
@@ -68,7 +71,7 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
         v5 = float(dsList[5])
         d['dataString'] = "%f %f %f %f %s %s"%(v0, v1, v2, v3, v4, v5)
         tforms[0].from_dict(d)
-
+        print "C"
         stackbounds = render.run(
                             renderapi.stack.get_bounds_from_z,
                             input_stack,
@@ -79,8 +82,10 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
                             z)
         tx =  int(stackbounds['minX']) - int(prestackbounds['minX'])
         ty =  int(stackbounds['minY']) - int(prestackbounds['minY'])
+        print "D"
         tforms1 = highres_ts.tforms
-        d = tforms1[0].to_dict()
+        d = tforms1[-1].to_dict()
+        print d
         dsList = d['dataString'].split()
         v0 = 1.0
         v1 = 0.0
@@ -89,10 +94,16 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
         v4 = tx
         v5 = ty
         d['dataString'] = "%f %f %f %f %s %s"%(v0,v1,v2,v3, v4,v5)
-        tforms1[0].from_dict(d)
+        tforms1[-1].from_dict(d)
+        print "E"
+
         ftform = tforms1 + tforms
+        print "F"
         allts = []
-        highres_ts1 = renderapi.tilespec.get_tile_specs_from_z(stack,z,render=render)
+        highres_ts1 = render.run(
+                            renderapi.tilespec.get_tile_specs_from_z,
+                            input_stack,
+                            z)
         for t in highres_ts1:
             print "f1"
             t.tforms.append(ftform)
@@ -103,8 +114,8 @@ def apply_rough_alignment(render, input_stack, prealigned_stack, lowres_stack, o
             d1['z'] = newz
             t.from_dict(d1)
             allts.append(t)
-            print d1
-            print "G"
+            
+        print "G"
         tilespecfilename = os.path.join(output_dir,'tilespec_%04d.json'%newz)
         print tilespecfilename
         fp = open(tilespecfilename,'w')
@@ -162,7 +173,7 @@ class ApplyRoughAlignmentTransform(RenderModule):
     def run(self):
         allzvalues = self.render.run(
             renderapi.stack.get_z_values_for_stack,
-            self.args['input_stack'])
+            self.args['montage_stack'])
         allzvalues = np.array(allzvalues)
         zvalues = allzvalues[(allzvalues >= self.args['minZ']) & (allzvalues <= self.args['maxZ'])]
 
@@ -177,7 +188,7 @@ class ApplyRoughAlignmentTransform(RenderModule):
         mypartial = partial(
                         apply_rough_alignment,
                         self.render,
-                        self.args['input_stack'],
+                        self.args['montage_stack'],
                         self.args['prealigned_stack'],
                         self.args['lowres_stack'],
                         self.args['tilespec_directory'],
