bb5e8d5a0b30fe144ba538e8c9432ff41373803e
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-09-15 10:06:59 -0700

    partial script for generating point matches

--- rendermodules/pointmatch/generate_point_matches.py
+++ rendermodules/pointmatch/generate_point_matches.py
@@ -21,12 +21,12 @@ example = {
         "client_scripts": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/render_20170613/render-ws-java-client/src/main/scripts"
     },
     "sparkhome": "/allen/programs/celltypes/workgroups/em-connectomics/ImageProcessing/utils/spark/",
-    "spinup_spark_pbs":"/allen/aibs/shared/image_processing/volume_assembly/utils/code/spark-submit/spinup_spark.pbs",
+    "spinup_spark_pbs":"/allen/aibs/pipeline/image_processing/volume_assembly/utils/code/spark-submit/spinup_spark.pbs",
     "logdir": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/scratch/sparkLogs/",
     "jarfile": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/render_20170613/render-ws-spark-client/target/render-ws-spark-client-0.3.0-SNAPSHOT-standalone.jar",
     "className":"org.janelia.render.client.spark.SIFTPointMatchClient",
     "baseDataUrl":"http://em-131fs:8998/render-ws/v1",
-    "owner": "gayathri",
+    "owner": "gayathri_MM2",
     "collection": "mm2_rough_align_test",
     "pairJson": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/scratch/rough/tilePairs/tile_pairs_mm2_montage_scape_test_z_1015_to_1035_dist_5.json",
     "SIFTfdSize": 8,
@@ -55,42 +55,52 @@ class SIFTPointMatchClientParameters(DefaultSchema):
     SIFTfdSize = Int(
         required=False,
         default=8,
+        missing=8,
         description="SIFT feature descriptor size: how many samples per row and column")
     SIFTsteps = Int(
         required=False,
         default=3,
+        missing=3,
         description="SIFT steps per scale octave")
     matchMaxEpsilon = Float(
         required=False,
         default=20.0,
+        missing=20.0,
         description="Minimal allowed transfer error for match filtering")
     maxFeatureCacheGb = Int(
         required=False,
         default=15,
+        missing=15,
         description="Maximum memory (in Gb) for caching SIFT features")
     SIFTminScale = Float(
         required=False,
         default=0.38,
+        missing=0.38,
         description="SIFT minimum scale: minSize * minScale < size < maxSize * maxScale")
     SIFTmaxScale = Float(
         required=False,
         default=0.82,
+        missing=0.82,
         description="SIFT maximum scale: minSize * minScale < size < maxSize * maxScale")
     renderScale = Float(
         required=False,
         default=0.3,
+        missing=0.3,
         description="Render canvases at this scale")
     matchRod = Float(
         required=False,
         default=0.92,
+        missing=0.92,
         description="Ratio of distances for matches")
     matchMinInlierRatio = Float(
         required=False,
         default=0.0,
+        missing=0.0,
         description="Minimal ratio of inliers to candidates for match filtering")
     matchMinNumInliers = Int(
         required=False,
         default=8,
+        missing=8,
         description="Minimal absolute number of inliers for match filtering")
     matchMaxNumInliers = Int(
         required=False,
@@ -101,10 +111,12 @@ class PointMatchClientParameters(RenderParameters):
     sparkhome = Str(
         required=False,
         default="/allen/aibs/shared/image_processing/volume_assembly/utils/spark",
+        missing="/allen/aibs/shared/image_processing/volume_assembly/utils/spark",
         description="Path to the spark home directory")
     logdir = Str(
         required=False,
         default="/allen/aibs/shared/image_processing/volume_assembly/logs/spark_logs",
+        missing="/allen/aibs/shared/image_processing/volume_assembly/logs/spark_logs",
         description="Directory to store spark logs")
     jarfile = Str(
         required=True,
@@ -119,7 +131,8 @@ class GeneratePointMatchesSpark(RenderModule):
         super(TilePairClientModule, self).__init__(
             schema_type=schema_type, *args, **kwargs)
 
-
+    def run(self):
+        
 
 
 if __name__ == "__main__":
