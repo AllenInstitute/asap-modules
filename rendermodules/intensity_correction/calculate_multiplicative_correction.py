2136bd68d676d8bceaefc2178a7f6507ada09613
forrest collman <forrest.collman@gmail.com>
2017-09-19 08:16:46 -0700

    fixed package name

--- rendermodules/intensity_correction/calculate_multiplicative_correction.py
+++ rendermodules/intensity_correction/calculate_multiplicative_correction.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__" and __package__ is None:
-    __package__ = "renderapps.intensity_correction.calculate_multiplicative_correction"
+    __package__ = "rendermodules.intensity_correction.calculate_multiplicative_correction"
 import json
 import os
 import renderapi
