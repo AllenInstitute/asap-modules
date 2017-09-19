44a5cae5f2ffbfc734e22ab67ddb8846d1733d14
forrest collman <forrest.collman@gmail.com>
2017-09-19 11:38:21 -0700

    added test for template module

---
+++ tests/test_template_module.py
@@ -0,0 +1,25 @@
+from rendermodules.module.template_module import TemplateModule
+import pytest
+import json
+
+def test_template_module(tmpdir):
+    tmpfile = tmpdir.join('output.json')
+    d = {
+        "render":{
+            "host": "ibs-forrestc-ux1",
+            "port": 8080,
+            "owner": "NewOwner",
+            "project": "H1706003_z150",
+            "client_scripts": "/pipeline/render/render-ws-java-client/src/main/scripts"
+        },
+        "example":"my example parameters",
+        "output_json":str(tmpfile)
+    }
+    mod = TemplateModule(input_data = d, args = [])
+    mod.run()
+
+    with open(str(tmpfile),'r') as fp:
+        output_d = json.load(fp)
+    
+    assert output_d['output_value']==(mod.args['example']+mod.args['default_val'])
+    
