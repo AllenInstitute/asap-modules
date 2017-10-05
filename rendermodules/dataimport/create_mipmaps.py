94385e4be30f3cb71cb5e0537f0f188d4e68d253
forrest collman <forrest.collman@gmail.com>
2017-10-05 06:12:06 -0700

    adding documentation string

--- rendermodules/dataimport/create_mipmaps.py
+++ rendermodules/dataimport/create_mipmaps.py
@@ -5,6 +5,33 @@ import os
 
 def create_mipmaps(inputImage, outputDirectory='.', mipmaplevels=[1, 2, 3],
                    outputformat='tif', convertTo8bit=True, force_redo=True):
+    """function to create downsampled images from an input image
+
+    Parameters
+    ==========
+    inputImage: str
+        path to input image
+    outputDirectory: str
+        path to save output images (default to current directory)
+    mipmaplevels: list or tuple
+        list or tuple of integers (default to (1,2,3))
+    outputformat: str
+        string representation of extension of image format (default tif)
+    convertTo8bit: boolean
+        whether to convert the image to 8 bit, dividing each value by 255
+    force_redo: boolean
+        whether to recreate mip map images if they already exist
+    
+    Returns
+    =======
+    list
+        list of output images created in order of levels
+    
+    Raises
+    ======
+    MipMapException
+        if an image cannot be created for some reason
+    """
     # Need to check if the level 0 image exists
     im = Image.open(inputImage)
     origsize = im.size
