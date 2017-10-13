0b05820027a86c028e6c22eb43a47c4d7890ae51
RussTorres <russtorres@gmail.com>
2017-10-13 15:56:18 -0700

    schemas: actually fix schema test

--- rendermodules/stack/schemas/__init__.py
+++ rendermodules/stack/schemas/__init__.py
@@ -1,4 +1,4 @@
 from consolidate_transforms_schemas import *
-import consolidate_transforms_schemas.__all__ as _all_ct
+from consolidate_transforms_schemas import __all__ as _all_ct
 
 __all__ = _all_ct
