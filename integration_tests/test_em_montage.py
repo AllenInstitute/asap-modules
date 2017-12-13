e72d0ad5b5b686c73aae3852ba09fd6f77f5c1f0
Forrest Collman <forrest.collman@gmail.com>
2017-12-13 09:56:24 -0800

    fixing glob to use first item in list

--- integration_tests/test_em_montage.py
+++ integration_tests/test_em_montage.py
@@ -85,7 +85,7 @@ def test_create_montage_tile_pairs(render, raw_stack, tmpdir_factory):
 @pytest.fixture(scope='module')
 def test_point_match_generation(render, test_create_montage_tile_pairs):
     pt_match_collection = 'montage_align_point_matches'
-    jarfile = glob.glob('/shared/render/render-ws-spark-client/target/render-ws-spark-client*SNAPSHOT-standalone.jar')
+    jarfile = glob.glob('/shared/render/render-ws-spark-client/target/render-ws-spark-client*SNAPSHOT-standalone.jar')[0]
     pointmatch_example['render'] = render_params
     pointmatch_example['pairJson'] = test_create_montage_tile_pairs
     pointmatch_example['owner'] = render_params['owner']
