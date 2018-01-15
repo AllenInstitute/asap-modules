943bbd268c0655b198d8116410f520402a2acdd5
Gayathri Mahalingam <mahalingamg@uncw.edu>
2018-01-15 14:09:42 -0800

    em montage qc working commit

--- rendermodules/em_montage_qc/schemas.py
+++ rendermodules/em_montage_qc/schemas.py
@@ -1,6 +1,8 @@
 import marshmallow as mm
+import argschema
+from marshmallow import post_load
 from ..module.render_module import RenderParameters
-from argschema.fields import Bool, Float, Int, Str, InputDir
+from argschema.fields import Bool, Float, Int, Str, InputDir, List
 
 
 class DetectMontageDefectsParameters(RenderParameters):
@@ -10,17 +12,40 @@ class DetectMontageDefectsParameters(RenderParameters):
     poststitched_stack = Str(
         required=True,
         description='Stitched montage stack')
-    zstart = Int(
+    match_collection = Str(
+        reuqired=True,
+        description='Name of the montage point match collection')
+    match_collection_owner = Str(
+        required=False,
+        default=None,
+        missing=None,
+        description='Name of the match collection owner')
+    minZ = Int(
         required=True,
         description='start z index of section')
-    zend = Int(
+    maxZ = Int(
         required=True,
         description='end z index of section')
+    residual_threshold = Int(
+        required=False,
+        default=6,
+        missing=6,
+        description='threshold value to filter residuals for detecting seams (default = 4)')
+    neighbors_distance = Int(
+        required=False,
+        default=60,
+        missing=60,
+        description='distance in pixels to look for neighboring points in seam detection (default = 60)')
+    min_cluster_size = Int(
+        required=False,
+        default=7,
+        missing=7,
+        description='minimum number of point matches required in each cluster for taking it into account for seam detection (default = 7)')
     plot_sections = Bool(
         required=False,
         default=False,
         missing=False,
-        description="Do you want to plot the sections with defects?. Will plot Bokeh plots in a html file")
+        description="Do you want to plot the sections with defects (holes or gaps)?. Will plot Bokeh plots in a html file")
     out_html = InputDir(
         required=False,
         default=None,
@@ -31,3 +56,30 @@ class DetectMontageDefectsParameters(RenderParameters):
         default=10,
         missing=10,
         description='Number of cores for parallel processing')
+    
+    @post_load
+    def add_match_collection_owner(self, data):
+        if data['match_collection_owner'] is None:
+            data['match_colelction_owner'] = data['render']['owner']
+
+class DetectMontageDefectsParametersOutput(argschema.schemas.DefaultSchema):
+    output_html = Str(
+        required=False,
+        default=None,
+        description='Output html file with Bokeh plots showing holes and stitching gaps')
+    hole_sections = argschema.fields.List(
+        argschema.fields.Int(), 
+        required=True, 
+        description='List of z values which failed QC and has holes')
+    gap_sections = argschema.fields.List(
+        argschema.fields.Int(),
+        required=True,
+        description='List of z values which have stitching gaps')
+    seam_sections = argschema.fields.List(
+        argschema.fields.Int(),
+        required=True,
+        description='List of z values which have seams detected')
+    seam_centroids = argschema.fields.List(
+        argschema.fields.List(argschema.fields.Float()),
+        required=False,
+        description='List of (x,y) positions of seams for each section')
