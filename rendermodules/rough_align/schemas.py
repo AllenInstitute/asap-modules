1391abcac5a75637d9165a543926509b3d74b91b
Forrest Collman <forrest.collman@gmail.com>
2018-02-04 09:49:31 -0800

    added output stack

--- rendermodules/rough_align/schemas.py
+++ rendermodules/rough_align/schemas.py
@@ -428,4 +428,4 @@ class SolveRoughAlignmentParameters(RenderParameters):
 
 class ApplyRoughAlignmentOutputParameters(DefaultSchema):
     zs = argschema.fields.NumpyArray(description="list of z values that were applied to")
-    
+    output_stack = argschema.fields.Str(descriptoin="stack where applied trasforms were set")
