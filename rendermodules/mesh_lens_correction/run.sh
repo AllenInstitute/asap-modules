a889f85ed2cb15d3cfb5849fff6e89eeb4b20608
Dan Kapner <danielk@.alleninstitute.org>
2018-06-22 15:06:01 -0700

    moving in files from em_lens_correction repo

---
+++ rendermodules/mesh_lens_correction/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#PBS -q emconnectome
+#PBS -l nodes=1:ppn=32 -n
+#PBS -l walltime=03:00:00
+#PBS -N em_lens_correction
+#PBS -r n
+#PBS -j oe
+#PBS -o /allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/log/pbs.log
+
+master_json=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/master.json
+z_set=100
+sectionid=tmp_sectionId
+stack=raw_lens_stack
+aligned_stack=aligned_corrected_lens_stack
+
+dvar=$(date "+%Y%m%d%H%M%S")
+tspecout=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/lens_outputs/tspec_out_${dvar}.json
+tpairout=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/lens_outputs/tpair_out_${dvar}.json
+lpmout=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/lens_outputs/lpm_out_${dvar}.json
+meshout=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/lens_outputs/mesh_out_${dvar}.json
+qcout=/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/qc/qc_out_${dvar}.json
+
+#set java variables for render-modules
+export RENDER_CLIENT_JAR=/allen/aibs/pipeline/image_processing/volume_assembly/render-jars/dev/render-ws-java-client-standalone.jar
+if [[ -v PBS_JOBID ]];
+then
+    export RENDER_JAVA_HOME=/shared/utils.x86_64/jdk_8u91
+    export JAVA_HOME=/shared/utils.x86_64/jdk_8u91
+    module load anaconda/4.3.1
+    module load java
+else
+    export RENDER_JAVA_HOME=/usr
+    export JAVA_HOME=/usr
+fi
+
+source activate /allen/programs/celltypes/workgroups/em-connectomics/danielk/conda/render-modules-lens
+
+#make a new stack section with the lc tiles
+python -m rendermodules.dataimport.generate_EM_tilespecs_from_metafile --input_json ${master_json} --output_json ${tspecout} --z ${z_set} --sectionId ${sectionid} --output_stack ${stack}
+#create tilepairs
+python -m rendermodules.pointmatch.create_tilepairs --input_json ${master_json} --z ${z_set} --zNeighborDistance 0 --xyNeighborFactor 0.1 --excludeCornerNeighbors False --excludeSameLayerNeighbors False --excludeCompletelyObscuredTiles True --output_json ${tpairout} --stack ${stack}
+
+#find pointmatches
+cd /allen/programs/celltypes/workgroups/em-connectomics/danielk
+python -m em_lens_correction.LensPointMatches --input_json ${master_json} --tilepair_output ${tpairout} --output_json ${lpmout} --stack ${stack}
+
+#find the lens correction, write out to new stack
+cd /allen/programs/celltypes/workgroups/em-connectomics/danielk/
+python -m em_lens_correction.MeshAndSolveTransform --input_json ${master_json} --sectionId ${sectionid} --output_json ${meshout} --stack ${stack} --aligned_stack ${aligned_stack}
+
+#run montage qc on the new stack
+python -m rendermodules.em_montage_qc.detect_montage_defects --input_json ${master_json} --prestitched_stack ${stack} --poststitched_stack ${aligned_stack} --minZ ${z_set} --maxZ ${z_set} --output_json ${qcout}
+
---
+++ rendermodules/mesh_lens_correction/schema.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+from argschema import ArgSchema
+from argschema.fields import String, Int, Boolean, Nested, Float
+from marshmallow import ValidationError
+
+
+class render(ArgSchema):
+    owner = String(
+        deafult="",
+        required=False,
+        description="owner")
+    project = String(
+        deafult="",
+        required=False,
+        description="project")
+    host = String(
+        default=None,
+        required=False,
+        description="render host")
+    port = Int(
+        default=8080,
+        required=False,
+        description="render port")
+    client_scripts = String(
+        deafult="/allen/aibs/pipeline/image_processing/volume_assembly/render-jars/production/scripts",
+        required=False,
+        description="render bin path")
+
+    def validate_data(self, data):
+        if data["host"] is None:
+            raise ValidationError("Need render host")
+
+
+class regularization(ArgSchema):
+    default_lambda = Float(
+        default=0.005,
+        description="regularization factor")
+    translation_factor = Float(
+        default=0.005,
+        description="regularization factor")
+    lens_lambda = Float(
+        default=0.005,
+        description="regularization for lens parameters")
+
+
+class RawLensStackSchema(ArgSchema):
+    render = Nested(render)
+    stack = String(
+        deafult="",
+        description="nameof stack for writing raw tilespecs")
+    metafile = String(
+        deafult="",
+        description="fullpath of metadata file")
+    tilepair_dir = String(
+        deafult="",
+        description="os.path.basename of metadata file")
+    overwrite_zlayer = Boolean(
+        default=True,
+        description="")
+    pool_size = Int(
+        default=10,
+        description="")
+    close_stack = Boolean(
+        default=True,
+        description="")
+    z = Int(
+        default=1,
+        description="")
+
+
+class LensPointMatchSchema(RawLensStackSchema):
+    match_collection = String(
+        deafult="",
+        description="name of point match collection")
+    tilepair_output = String(
+        deafult="",
+        description="path to tilepair json file")
+    pairJson = String(
+        deafult="",
+        description="path to tilepair json file")
+    nfeature_limit = Int(
+        default=10000,
+        required=False,
+        description="randomly choose this many features per tile")
+    matchMax = Int(
+        default=1000,
+        required=False,
+        description="maximum pointmatches per tile pair")
+    ncpus = Int(
+        default=-1,
+        required=False,
+        description="max number of cpus to use")
+    downsample_scale = Float(
+        default=0.3,
+        required=False,
+        description="passed to cv2.resize fx,fy")
+
+
+class LensCorrectionSchema(LensPointMatchSchema):
+    aligned_stack = String(
+        deafult="",
+        description="name \
+        of stack after correction and alignment")
+    nvertex = Int(
+        default=1000,
+        required=True,
+        description="maximum number of vertices to attempt")
+    sectionId = String(
+        deafult="",
+        description="sectionId for creating correction")
+    render_output = String(
+        deafult="null",
+        description="path, null, or stdout")
+    output_dir = String(
+        deafult="",
+        description="write lens correction tform jsons here")
+    out_html_dir = String(
+        deafult="",
+        description="where to write some qc files")
+    regularization = Nested(regularization)
