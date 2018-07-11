d2207af92dcea5c4f1153119845ff562d5bfbf4d
Dan Kapner <32312979+djkapner@users.noreply.github.com>
2018-07-11 09:59:34 -0700

    Add coarse mesh failure (#130)
    * adding failure for coarse mesh
    
    * adding failure for coarse mesh
    
    * adding test for data that has bad contrast in corners
    
    * reintroducing argchema to the MeshAndSolve class, making some functions more explicit
    
    * refactor for improved exception handling, logging, and testing
    
    * found 2 other self. usages
    
    * changing main mathod to name run()
    
    * eliminate unused method, some flake8
    
    * moved functions outside class, added rerun_pointmatch Bool, some formatting
    
    * edging up code coverage, trying to remove some reruns for speed
    
    * fixing z/groupId confusion, edging up coverage, reducing test time
    
    * corrected test_mesh match import
    
    * corrected test_mesh render in match import
    
    * trying to cover delete pointmatches...
    
    * get_matchcollections does not return names
    
    * moving to logging from print(), not modifying example args directly
    
    * implementing output schema for MeshAndSolve
    
    * resolving output_json being overwrittenand tmpdir issue in test
    
    * replacing asserts with ifs

--- integration_tests/test_mesh_lens_correction.py
+++ integration_tests/test_mesh_lens_correction.py
@@ -4,7 +4,7 @@ import renderapi
 import os
 
 from rendermodules.mesh_lens_correction.do_mesh_lens_correction import MeshLensCorrection
-from rendermodules.mesh_lens_correction.LensPointMatches import generate_point_matches
+from rendermodules.mesh_lens_correction.MeshAndSolveTransform import MeshLensCorrectionException
 from test_data import render_params, TEST_DATA_ROOT
 
 example = {
@@ -41,24 +41,66 @@ def render():
     return render
 
 
+def test_coarse_mesh_failure(render, tmpdir_factory):
+    outdir = str(tmpdir_factory.mktemp("mesh_lens"))
+    out_html_dir = outdir
+    example_for_input = dict(example)
+    example_for_input['render'] = render_params
+    example_for_input['metafile'] = os.path.join(TEST_DATA_ROOT,
+                                                 "em_modules_test_data",
+                                                 "mesh_lens_correction_3",
+                                                 "mesh_lens_metafile_3.json")
+    example_for_input['output_dir'] = outdir
+    example_for_input['out_html_dir'] = out_html_dir
+    example_for_input['z_index'] = 101
+    outjson = os.path.join(outdir, 'mesh_lens_out0.json')
+
+    mod = MeshLensCorrection(
+            input_data=example_for_input,
+            args=['--output_json', outjson])
+
+    with pytest.raises(MeshLensCorrectionException):
+        mod.run()
+
+
 def test_mesh_lens_correction(render, tmpdir_factory):
     outdir = str(tmpdir_factory.mktemp("mesh_lens"))
     out_html_dir = outdir
-    example['render'] = render_params
-    example['metafile'] = os.path.join(TEST_DATA_ROOT,
-                                       "em_modules_test_data",
-                                       "mesh_lens_correction_2",
-                                       "mesh_lens_metafile_2.json")
-    example['output_dir'] = outdir
-    example['out_html_dir'] = out_html_dir
-    example['z_index'] = 100
+    example_for_input = dict(example)
+    example_for_input['render'] = render_params
+    example_for_input['metafile'] = os.path.join(TEST_DATA_ROOT,
+                                                 "em_modules_test_data",
+                                                 "mesh_lens_correction_2",
+                                                 "mesh_lens_metafile_2.json")
+    example_for_input['output_dir'] = outdir
+    example_for_input['out_html_dir'] = out_html_dir
+    example_for_input['outfile'] = os.path.join(outdir, 'out.json')
+    outjson = os.path.join(outdir, 'mesh_lens_out.json')
+    example_for_input['z_index'] = 100
 
     mod = MeshLensCorrection(
-            input_data=example,
-            args=['--output_json', 'mesh_lens_out.json'])
+            input_data=example_for_input,
+            args=['--output_json', outjson])
+
+    # add collection and a match so we cover deletion
+    sectionId = mod.get_sectionId_from_metafile(example_for_input['metafile'])
+    match = {}
+    match['qId'] = 'qid'
+    match['pId'] = 'pid'
+    match['pGroupId'] = sectionId
+    match['qGroupId'] = sectionId
+    match['matches'] = {}
+    match['matches']['q'] = [[0], [1]]
+    match['matches']['p'] = [[2], [3]]
+    match['matches']['w'] = [0]
+    renderapi.pointmatch.import_matches(
+            example_for_input['match_collection'],
+            [match],
+            render=mod.render)
+
     mod.run()
 
-    with open('mesh_lens_out.json', 'r') as f:
+    with open(outjson, 'r') as f:
         js = json.load(f)
 
     with open(js['output_json'], 'r') as f:
@@ -71,14 +113,8 @@ def test_mesh_lens_correction(render, tmpdir_factory):
             new_tform_dict['className'] ==
             "mpicbg.trakem2.transform.ThinPlateSplineTransform")
 
-    # run again for code coverage
+    # some little bits of coverage
+    mod.args['rerun_pointmatch'] = False
+    mod.args['output_dir'] = None
+    mod.args['outfile'] = None
     mod.run()
-
-    # more code coverage due,
-    # code coverage software not detecting parallel processes
-
-    generate_point_matches(mod.render,
-                           mod.args['pairJson'],
-                           mod.args['input_stack'],
-                           mod.args['match_collection'],
-                           mod.args['matchMax'])
--- rendermodules/mesh_lens_correction/do_mesh_lens_correction.py
+++ rendermodules/mesh_lens_correction/do_mesh_lens_correction.py
@@ -1,16 +1,21 @@
-import os
 import json
 import renderapi
 import tempfile
 
 from ..module.render_module import RenderModule
 
-from rendermodules.mesh_lens_correction.schemas import MeshLensCorrectionSchema, MeshLensCorrectionOutputSchema
-from rendermodules.dataimport.generate_EM_tilespecs_from_metafile import GenerateEMTileSpecsModule
-from rendermodules.pointmatch.create_tilepairs import TilePairClientModule
-from rendermodules.mesh_lens_correction.LensPointMatches import generate_point_matches
-from rendermodules.mesh_lens_correction.MeshAndSolveTransform import MeshAndSolveTransform
-from rendermodules.em_montage_qc.detect_montage_defects import DetectMontageDefectsModule
+from rendermodules.mesh_lens_correction.schemas \
+        import MeshLensCorrectionSchema, DoMeshLensCorrectionOutputSchema
+from rendermodules.dataimport.generate_EM_tilespecs_from_metafile \
+        import GenerateEMTileSpecsModule
+from rendermodules.pointmatch.create_tilepairs \
+        import TilePairClientModule
+from rendermodules.mesh_lens_correction.LensPointMatches \
+        import generate_point_matches
+from rendermodules.mesh_lens_correction.MeshAndSolveTransform \
+        import MeshAndSolveTransform
+from rendermodules.em_montage_qc.detect_montage_defects \
+        import DetectMontageDefectsModule
 
 example = {
     "render": {
@@ -18,8 +23,8 @@ example = {
         "port": 8080,
         "owner": "gayathrim",
         "project": "lens_corr",
-        "client_scripts":"/allen/aibs/pipeline/image_processing/volume_assembly/render-jars/production/scripts",
-        "memGB":"2G"
+        "client_scripts": "/allen/aibs/pipeline/image_processing/volume_assembly/render-jars/production/scripts",
+        "memGB": "2G"
     },
     "regularization": {
         "default_lambda": 1e5,
@@ -32,15 +37,33 @@ example = {
     "close_stack": True,
     "z_index": 100,
     "metafile": "/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_lens_correction/test_data/_metadata_20180220175645_247488_8R_tape070A_05_20180220175645_reference_0_.json",
-    "match_collection":"raw_lens_matches",
+    "match_collection": "raw_lens_matches",
     "nfeature_limit": 20000,
     "output_dir": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/scratch",
     "outfile": "lens_out.json"
 }
 
+
+def delete_matches_if_exist(render, owner, collection, sectionId):
+    collections = renderapi.pointmatch.get_matchcollections(
+            render=render,
+            owner=owner)
+    collection_names = [c['collectionId']['name'] for c in collections]
+    if collection in collection_names:
+        groupids = renderapi.pointmatch.get_match_groupIds(
+                collection,
+                render=render)
+        if sectionId in groupids:
+            renderapi.pointmatch.delete_point_matches_between_groups(
+                    collection,
+                    sectionId,
+                    sectionId,
+                    render=render)
+
+
 class MeshLensCorrection(RenderModule):
     default_schema = MeshLensCorrectionSchema
-    default_output_schema = MeshLensCorrectionOutputSchema
+    default_output_schema = DoMeshLensCorrectionOutputSchema
 
     @staticmethod
     def get_sectionId_from_z(z):
@@ -68,7 +91,6 @@ class MeshLensCorrection(RenderModule):
         ex['z'] = self.args['z_index']
         ex['sectionId'] = self.args['sectionId']
         return ex
-
 
     def generate_tilepair_example(self):
         ex = {}
@@ -108,7 +130,8 @@ class MeshLensCorrection(RenderModule):
         return ex
 
     def run(self):
-        self.args['sectionId'] = self.get_sectionId_from_metafile(self.args['metafile'])
+        self.args['sectionId'] = self.get_sectionId_from_metafile(
+                self.args['metafile'])
 
         if self.args['output_dir'] is None:
             self.args['output_dir'] = tempfile.mkdtemp()
@@ -120,19 +143,10 @@ class MeshLensCorrection(RenderModule):
             outfile.close()
             self.args['outfile'] = outfile.name
 
-        # check if match_collection exists
-        collections = renderapi.pointmatch.get_matchcollections(render=self.render)
-        if self.args['match_collection'] in collections:
-            # check if the point match exists
-            groupids = renderapi.pointmatch.get_match_groupIds(self.args['match_collection'], render=self.render)
-            if str(float(self.args['z_index'])) in groupids:
-                renderapi.pointmatch.delete_point_matches_between_groups(self.args['match_collection'],
-                                                                        str(float(self.args['z_index'])),
-                                                                        str(float(self.args['z_index'])),
-                                                                        render=self.render)
-
         out_file = tempfile.NamedTemporaryFile(suffix=".json", delete=False)
         out_file.close()
+
+        args_for_input = dict(self.args)
 
         # create a stack with the lens correction tiles
         ts_example = self.generate_ts_example()
@@ -143,46 +157,54 @@ class MeshLensCorrection(RenderModule):
         # generate tile pairs for this section in the input stack
         tp_example = self.generate_tilepair_example()
         tp_mod = TilePairClientModule(input_data=tp_example,
-                                        args=['--output_json', out_file.name])
+                                      args=['--output_json', out_file.name])
         tp_mod.run()
 
         with open(tp_mod.args['output_json'], 'r') as f:
             js = json.load(f)
         self.args['pairJson'] = js['tile_pair_file']
 
-        # generate point matches
-        generate_point_matches(self.render,
-                               self.args['pairJson'],
-                               self.args['input_stack'],
-                               self.args['match_collection'],
-                               self.args['matchMax'],
-                               nfeature_limit=self.args['nfeature_limit'])
+        self.logger.setLevel(self.args['log_level'])
 
-        meshclass = MeshAndSolveTransform()
+        if self.args['rerun_pointmatch']:
+            delete_matches_if_exist(
+                    self.render,
+                    self.args['render']['owner'],
+                    self.args['match_collection'],
+                    self.args['sectionId'])
+
+            # generate point matches
+            generate_point_matches(self.render,
+                                   self.args['pairJson'],
+                                   self.args['input_stack'],
+                                   self.args['match_collection'],
+                                   self.args['matchMax'],
+                                   nfeature_limit=self.args['nfeature_limit'],
+                                   logger=self.logger)
+
+        self.logger.setLevel(self.args['log_level'])
+
+        meshclass = MeshAndSolveTransform(
+                input_data=args_for_input,
+                args=['--output_json', out_file.name])
         # find the lens correction, write out to new stack
-        lens_correction_json = meshclass.MeshAndSolve(self.render,
-                                            self.args['input_stack'],
-                                            self.args['output_stack'],
-                                            self.args['match_collection'],
-                                            self.args['sectionId'],
-                                            self.args['nvertex'],
-                                            self.args['output_dir'],
-                                            self.args['outfile'],
-                                            self.args['regularization']['default_lambda'],
-                                            self.args['regularization']['translation_factor'],
-                                            self.args['regularization']['lens_lambda'],
-                                            self.args['close_stack'])
+        meshclass.run()
 
         # run montage qc on the new stack
         qc_example = self.get_qc_example()
-        qc_mod = DetectMontageDefectsModule(input_data=qc_example, args=['--output_json', out_file.name])
+        qc_mod = DetectMontageDefectsModule(
+                input_data=qc_example,
+                args=['--output_json', out_file.name])
         qc_mod.run()
 
         try:
-            self.output({'output_json': lens_correction_json, 'qc_json': out_file.name})
+            self.output(
+                    {'output_json': meshclass.args['outfile'],
+                     'qc_json': out_file.name})
         except AttributeError as e:
             self.logger.error(e)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     mod = MeshLensCorrection(input_data=example)
     mod.run()
