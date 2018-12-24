f7a5ab9f54dad1d6108948eecdcb92121e2932f2
RussTorres <russtorres@gmail.com>
2018-12-24 12:42:08 -0800

    validate_mat: add materialization file

--- rendermodules/materialize/validate_materialized_tilesource.py
+++ rendermodules/materialize/validate_materialized_tilesource.py
@@ -12,6 +12,11 @@ import imageio
 from rendermodules.materialize.schemas import (
     ValidateMaterializationParameters, ValidateMaterializationOutput)
 
+try:
+    xrange
+except NameError:
+    xrange = range
+
 
 example_input = {
     "minRow": 176,
@@ -54,18 +59,47 @@ class ValidateMaterialization(argschema.ArgSchemaParser):
     def try_load_tile(cls, tile_fn):
         return tile_fn if cls.try_load_file(tile_fn) is None else None
 
-    def build_tilefiles(self, basedir, minZ, maxZ, minRow, maxRow,
-                        minCol, maxCol, ext, mmL=0):
-        for z in range(minZ, maxZ + 1):
-            for r in range(minRow, maxRow + 1):
-                for c in range(minCol, maxCol + 1):
-                    yield self.tilefile_from_ts5(basedir, z, r, c, ext, mmL)
+    @staticmethod
+    def rows_from_ts5(basedir, z, mmL=0):
+        for d in os.listdir(os.path.join(basedir, str(mmL), str(z))):
+            if d.isdigit():
+                yield int(d)
+
+    @staticmethod
+    def cols_from_ts5(basedir, z, row, ext, mmL=0):
+        # TODO make ext work for filtering
+        for fn in os.listdir(os.path.join(
+                basedir, str(mmL), str(z), str(row))):
+            if os.path.splitext(fn)[0].isdigit():
+                yield int(os.path.splitext(fn)[0])
+
+    @classmethod
+    def build_tilefiles(cls, basedir, minZ, maxZ, minRow=None, maxRow=None,
+                        minCol=None, maxCol=None, ext="png", mmL=0):
+        for z in xrange(minZ, maxZ + 1):
+            if minRow is None or maxRow is None:
+                allrows = sorted(cls.rows_from_ts5(basedir, z, mmL))
+                if minRow is None:
+                    minRow = allrows[0]
+                if maxRow is None:
+                    maxRow = allrows[-1]
+            # this checks for thing that don't necessarily exist.
+            for r in xrange(minRow, maxRow + 1):
+                if minCol is None or maxCol is None:
+                    allcols = sorted(cls.cols_from_ts5(
+                        basedir, z, r, ext, mmL))
+                    if minCol is None:
+                        minCol = allcols[0]
+                    if maxCol is None:
+                        maxCol = allcols[-1]
+                for c in xrange(minCol, maxCol+1):
+                    yield cls.tilefile_from_ts5(basedir, z, r, c, ext, mmL)
 
     def run(self):
         tile_files = self.build_tilefiles(
             self.args['basedir'], self.args['minZ'], self.args['maxZ'],
-            self.args['minRow'], self.args['maxRow'],
-            self.args['minCol'], self.args['maxCol'], self.args['ext'])
+            self.args.get('minRow'), self.args.get('maxRow'),
+            self.args.get('minCol'), self.args.get('maxCol'), self.args['ext'])
         pool = ThreadPool(self.args.get('pool_size'))
         bad_files = filter(None, pool.imap_unordered(
             self.try_load_tile, tile_files))
