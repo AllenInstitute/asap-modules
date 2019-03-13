ddbe4f8ad48389e721d14c40ba2aa143c09061bf
Dan Kapner <danielk@.alleninstitute.org>
2019-03-12 18:41:20 -0700

    removing incorrect pytest fixture decorator

--- integration_tests/test_lens_correction.py
+++ integration_tests/test_lens_correction.py
@@ -112,6 +112,7 @@ def test_points(example_tform_dict):
     return test_points
 
 
+#@pytest.fixture(scope='module')
 def compute_lc_norm_and_max(test_example_tform, test_new_tform):
     tform_norm = (
             np.linalg.norm(test_new_tform - test_example_tform) /
