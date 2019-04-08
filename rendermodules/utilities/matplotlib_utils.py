c8f8a83c68772b33340e673135e9cda1695f70e9
Gayathri Mahalingam <mahalingamg@uncw.edu>
2019-04-08 16:02:34 -0700

    separate import statements

--- rendermodules/utilities/matplotlib_utils.py
+++ rendermodules/utilities/matplotlib_utils.py
@@ -1,10 +1,9 @@
 # imports needed for rough_align_qc
 import os
-import matplotlib
+import matplotlib as mpl
 if os.environ.get('DISPLAY', '') == '':
-    matplotlib.use('Agg')
+    mpl.use('Agg')
 
 import matplotlib.pyplot as plt
 import mpld3
-import matplotlib as mpl
 from matplotlib.backends.backend_pdf import PdfPages
