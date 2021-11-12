725d81f452d843666d0ad6cd7efb5049add5fa5a
Russel Torres <russtorres@gmail.com>
2021-11-12 11:15:56 -0800

    setup: formatting, new author order, email

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import logging
 
+
 class PyTest(TestCommand, object):
     user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
 
@@ -13,9 +14,11 @@ class PyTest(TestCommand, object):
 
     def run(self):
         try:
-            import cv2
-            import em_stitch
-            self.distribution.install_requires = [i for i in self.distribution.install_requires if not i.startswith('em_stitch')]
+            import cv2  # noqa: F401
+            import em_stitch  # noqa: F401
+            self.distribution.install_requires = [
+                i for i in self.distribution.install_requires
+                if not i.startswith('em_stitch')]
         except ImportError:
             pass
         print(self.distribution.install_requires)
@@ -39,17 +42,26 @@ with open('test_requirements.txt', 'r') as f:
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
-setup(name='render-modules',
+setup(name='asap-modules',
       use_scm_version=True,
-      description='A set of python modules for doing higher level processing steps used '
-                  ' render, developed for processing array tomography and EM images.'
-                  'see http://github.com/saalfeldlab/render',
-      author='Sharmishtaa Seshamani, Forrest Collman, Russel Torres, Gayathri Mahalingam, Sam Kinn',
-      author_email='forrestc@alleninstitute.org',
-      url='https://github.com/AllenInstitute/render-modules/',
+      description=(
+          'A set of python modules for doing higher level processing steps '
+          'using render, developed for processing array tomography '
+          'and EM images. See http://github.com/saalfeldlab/render'),
+      author=(
+          'Gayathri Mahalingam, Russel Torres, Daniel Kapner, '
+          'Sharmishtaa Seshamani, Forrest Collman, Eric Perlman, Sam Kinn'),
+      author_email='gayathrim@alleninstitute.org',
+      url='https://github.com/AllenInstitute/asap/',
       packages=find_packages(),
-      package_data={'':['*.bsh','LICENSE','README.rst','*.md', 'rendermodules/point_match_optimization/*.html']},
+      package_data={
+          '': [
+              '*.bsh',
+              'LICENSE',
+              'README.rst',
+              '*.md',
+              'asap/point_match_optimization/*.html']},
       install_requires=required,
-      setup_requires=['flake8','setuptools_scm'],
+      setup_requires=['flake8', 'setuptools_scm'],
       tests_require=test_required,
       cmdclass={'test': PyTest})
