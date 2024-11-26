2804a441c78bbc576255b2f2b615b86a8f07d6a6
Russel Torres <russtorres@gmail.com>
2024-11-26 13:58:58 -0800

    residuals: add compute residuals for object interface

--- asap/residuals/compute_residuals.py
+++ asap/residuals/compute_residuals.py
@@ -3,6 +3,59 @@ import requests
 import renderapi
 
 
+def compute_residuals(tilespecs, matches, min_points=1, extra_statistics=None):
+    """from compute_residuals_in_group for in-memory"""
+    extra_statistics = extra_statistics or {}
+
+    tId_to_tforms = {ts.tileId: ts.tforms for ts in tilespecs}
+
+    tile_residuals = {key: np.empty((0, 1)) for key in tId_to_tforms.keys()}
+    tile_rmse = {key: np.empty((0, 1)) for key in tId_to_tforms.keys()}
+    pt_match_positions = {key: np.empty((0, 2)) for key in tId_to_tforms.keys()}
+
+    statistics = {}
+
+    for match in matches:
+        pts_p = np.array(match['matches']['p'])
+        pts_q = np.array(match['matches']['q'])
+    
+        if pts_p.shape[1] < min_points:
+            continue
+
+        try:
+            t_p = tId_to_tforms[match['pId']][-1].tform(pts_p.T)
+            t_q = tId_to_tforms[match['qId']][-1].tform(pts_q.T)
+        except KeyError:
+            continue
+
+        positions = (t_p + t_q) / 2.
+    
+        res = np.linalg.norm(t_p - t_q, axis=1)
+        rmse = np.true_divide(res, res.shape[0])
+
+        tile_residuals[match['pId']] = np.append(
+            tile_residuals[match['pId']], res)
+        tile_rmse[match['pId']] = np.append(
+            tile_rmse[match['pId']], rmse)
+        pt_match_positions[match['pId']] = np.append(
+            pt_match_positions[match['pId']], positions, axis=0)
+
+    empty_keys = [k for k in tile_residuals if tile_residuals[k].size == 0]
+
+    for k in empty_keys:
+        tile_residuals.pop(k)
+        tile_rmse.pop(k)
+        pt_match_positions.pop(k)
+
+    statistics['tile_rmse'] = tile_rmse
+    statistics['tile_residuals'] = tile_residuals
+    statistics['pt_match_positions'] = pt_match_positions
+
+    statistics = {**extra_statistics, **statistics}
+    
+    return statistics, matches
+
+
 def compute_residuals_within_group(render, stack, matchCollectionOwner,
                                    matchCollection, z, min_points=1,
                                    tilespecs=None):
@@ -24,48 +77,10 @@ def compute_residuals_within_group(render, stack, matchCollectionOwner,
     if tilespecs is None:
         tilespecs = render.run(renderapi.tilespec.get_tile_specs_from_z,
                                stack, z, session=session)
-    tforms = {ts.tileId: ts.tforms for ts in tilespecs}
 
-    tile_residuals = {key: np.empty((0, 1)) for key in tforms.keys()}
-    tile_rmse = {key: np.empty((0, 1)) for key in tforms.keys()}
-    pt_match_positions = {key: np.empty((0, 2)) for key in tforms.keys()}
-
-    statistics = {}
-    for i, match in enumerate(allmatches):
-        pts_p = np.array(match['matches']['p'])
-        pts_q = np.array(match['matches']['q'])
-
-        if pts_p.shape[1] < min_points:
-            continue
-        try:
-            t_p = tforms[match['pId']][-1].tform(pts_p.T)
-            t_q = tforms[match['qId']][-1].tform(pts_q.T)
-        except KeyError:
-            continue
-
-        positions = (t_p + t_q) / 2.
-
-        res = np.linalg.norm(t_p - t_q, axis=1)
-        rmse = np.true_divide(res, res.shape[0])
-
-        tile_residuals[match['pId']] = np.append(
-            tile_residuals[match['pId']], res)
-        tile_rmse[match['pId']] = np.append(
-            tile_rmse[match['pId']], rmse)
-        pt_match_positions[match['pId']] = np.append(
-            pt_match_positions[match['pId']], positions, axis=0)
-
-    # remove empty entries from these dicts
-    empty_keys = [k for k in tile_residuals if tile_residuals[k].size == 0]
-    for k in empty_keys:
-        tile_residuals.pop(k)
-        tile_rmse.pop(k)
-        pt_match_positions.pop(k)
-
-    statistics['tile_rmse'] = tile_rmse
-    statistics['z'] = z
-    statistics['tile_residuals'] = tile_residuals
-    statistics['pt_match_positions'] = pt_match_positions
+    statistics, allmatches = compute_residuals(
+        tilespecs, allmatches, min_points=min_points,
+        extra_statistics={"z": z})
 
     session.close()
 
